49aad5938081e649872124418eaef11ed8820663
saadu1998 <1998saadullah@gmail.com>
2022-03-15 00:56:32 +0500

    pomodomo timmer

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ const generatingNewItem = (todo) => {
     const html = `<li
     class="list-group-item d-flex justify-content-between align-items-center"
   >
+  
     <span>${todo}</span>
+    
     <i class="bi bi-trash delete"></i>
-  </li>`
+  </li>
+  `
 
   list.innerHTML +=html ;
 
@@ -64,3 +67,127 @@ const filtering= ((term) => {
     .forEach((todo) => todo.classList.remove('filtered'))
 })
 
+// adding timer
+const countDown = document.querySelector('.countdown');
+const timeValue = document.querySelector('.timeValue')
+
+
+let startingMinutes= timeValue.timeValue.value;
+
+let time = startingMinutes * 60
+let interval;
+
+
+const timer = () => {
+     
+
+
+  const minutes= Math.floor(time/60);
+  let seconds= time % 60 ; 
+
+ 
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  countDown.innerHTML = ` <h1 class="timer-clock">
+  ${minutes} : ${seconds}
+</h1>`
+
+
+
+
+
+
+time--;
+
+
+
+
+
+
+
+}  
+
+
+
+
+const play = document.querySelector('.playTimer')
+const stopTimer = document.querySelector('.stopTimer')
+const resetTimer = document.querySelector('.resetTimer')
+const startButton = document.querySelector('.starting')
+const displayButton = document.querySelector('.ThreeButtons')
+
+
+startButton.addEventListener('click', (e)=> {
+
+  if(list.childElementCount){
+
+
+
+    e.preventDefault();
+    startingMinutes= timeValue.timeValue.value;
+    time = startingMinutes * 60;
+    
+  timer();
+  interval = setInterval(timer , 1000);
+  displayButton.classList.remove('d-none');
+  timeValue.reset();
+
+  } else{
+    alert('please add task')
+  }
+
+
+   
+
+    } )
+
+play.addEventListener('click', (e)=> {
+
+
+
+    e.preventDefault();
+
+    
+  timer();
+  interval = setInterval(timer , 1000);
+
+
+   
+
+    } )
+stopTimer.addEventListener('click', (e)=> {
+
+    e.preventDefault();
+    
+    
+  timer();
+clearInterval(interval)
+   
+
+    } )
+    resetTimer.addEventListener('click', (e)=> {
+
+    e.preventDefault();
+    
+    
+clearInterval(interval);
+
+countDown.innerHTML = ` <h1 class="timer-clock">00 :00</h1>`
+startingMinutes= timeValue.timeValue.value;
+    time = startingMinutes * 60;
+    displayButton.classList.add('d-none');
+   
+
+    } )
+
+
+
+   
+    
+
+
+
+  
+
+
+    
+   
